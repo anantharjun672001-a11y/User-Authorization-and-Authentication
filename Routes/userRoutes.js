380c003b63da9447a2cee45a5bb30258858313a1
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-03-30 14:31:45 +0530

    profile route created

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { loginUser, registerUser } from "../Controllers/userController.js";
+import { getUserProfile, loginUser, registerUser } from "../Controllers/userController.js";
+import { authMiddleware } from "../Middleware/authMiddleware.js";
 
 
 const router = express.Router();
@@ -7,5 +8,7 @@ const router = express.Router();
 router.post("/register",registerUser);
 
 router.post("/login",loginUser);
+
+router.get("/profile",authMiddleware,getUserProfile);
 
 export default router;
