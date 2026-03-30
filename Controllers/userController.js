c69cf2c3c75510697de3dd3577f816d104dbb03a
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-03-30 14:15:42 +0530

    login user created successfully

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -27,3 +27,29 @@ export const registerUser = async (req,res) => {
     }
 }
 
+// Login User
+
+export const loginUser = async (req,res) => {
+    try {
+        const {email,password} = req.body;
+
+        const user = await User.findOne({email});
+
+        if (!user) {
+            return res.status(400).json({ message: "User Not Found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn: "1h"});
+
+        res.status(200).json({ token });
+        
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
